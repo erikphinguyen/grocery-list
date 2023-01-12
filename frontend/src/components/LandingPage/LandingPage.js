192f1cf694e18552de5284e94573505dc2c6e63f
erikphinguyen <erikphinguyen@gmail.com>
2023-01-11 20:23:01 -0800

    starting landing page

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 function LandingPage() {
-  return (
-    <div>LandingPage</div>
-  )
+    return (
+        <div className='landing-page-container'>
+            <div>
+                <h1>GROCERY DAY?</h1>
+                <NavLink to={`users/:id/tasks`}>
+                    <button>LET'S MAKE A LIST</button>
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPage;
