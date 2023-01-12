1d60fac24708d3693b4263118f613d9562afad55
erikphinguyen <erikphinguyen@gmail.com>
2023-01-11 18:28:47 -0800

    finished post and edit task

--- backend/routes/api/tasks.js
+++ backend/routes/api/tasks.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
-const { requireAuth } = require('../../utils/auth.js');
+const { requireAuth, restoreUser } = require('../../utils/auth.js');
 const { User, Task } = require('../../db/models');
 
 // GET TASKS
@@ -15,6 +15,33 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     });
 
     return res.json(tasks);
+}))
+
+// POST TASK
+router.post('/', requireAuth, restoreUser, asyncHandler(async (req, res) => {
+    const { userId, task } = req.body;
+
+    const newTask = await Task.create({
+        userId,
+        task
+    });
+
+    return res.json(newTask);
+}))
+
+
+// EDIT TASK
+router.put('/:id(\\d+)', requireAuth, restoreUser, asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { task } = req.body;
+
+    const taskToEdit = await Task.findByPk(id);
+
+    taskToEdit.task = task;
+
+    await taskToEdit.save();
+
+    return res.json(taskToEdit);
 }))
 
 module.exports = router;
