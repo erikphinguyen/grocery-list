938ce307cb47091f11db13232fc6f407cacf3870
erikphinguyen <erikphinguyen@gmail.com>
2023-01-11 17:07:54 -0800

    finishing up task model, migration, and seeder

--- backend/db/models/task.js
+++ backend/db/models/task.js
@@ -1,10 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
+    userId: DataTypes.INTEGER,
     task: DataTypes.STRING
   }, {});
   Task.associate = function(models) {
-    // associations can be defined here
+    Task.belongsTo(models.User, { foreignKey: 'userId' })
   };
   return Task;
 };
