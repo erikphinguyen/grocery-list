884530f0ea762dc0e979e274c3adf496efdbbf98
erikphinguyen <erikphinguyen@gmail.com>
2023-01-11 18:25:45 -0800

    get tasks done

--- backend/routes/api/tasks.js
+++ backend/routes/api/tasks.js
@@ -2,4 +2,19 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { requireAuth } = require('../../utils/auth.js');
-const { User, Task} = require('../../db/models');
+const { User, Task } = require('../../db/models');
+
+// GET TASKS
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const tasks = await Task.findAll({
+        where: {
+            userId: id
+        }
+    });
+
+    return res.json(tasks);
+}))
+
+module.exports = router;
