ce8a62ad41ee6f7f22031b3d9e5e982bab54e341
erikphinguyen <erikphinguyen@gmail.com>
2023-01-12 23:42:27 -0800

    log in and sign up CSS done

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -20,7 +20,7 @@ function LoginForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form className='form' onSubmit={handleSubmit}>
             <ul>
                 {errors.map((error, idx) => (
                     <li key={idx}>{error}</li>
@@ -29,22 +29,26 @@ function LoginForm() {
             <label>
                 Username or Email
                 <input
+                className='input'
                     type="text"
                     value={credential}
                     onChange={(e) => setCredential(e.target.value)}
                     required
                 />
             </label>
+            <br></br>
             <label>
                 Password
                 <input
+                className='input'
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
             </label>
-            <button type="submit">Log In</button>
+            <br></br>
+            <button className='button' type="submit">Log In</button>
         </form>
     );
 }
